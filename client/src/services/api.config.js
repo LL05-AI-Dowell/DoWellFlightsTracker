f8289238b429a013aed2ffe6773881edda20057f
Manish <mdashsharma95@gmail.com>
2025-01-14 14:18:28 +0530

    added config url

--- client/src/services/api.config.js
+++ client/src/services/api.config.js
@@ -1,6 +1,10 @@
-import { servicesAxiosInstance } from './config'
+import { servicesAxiosInstance, tempAxiosInstance } from './config'
 
 export const getServerHealth = async () => {
     const response = await servicesAxiosInstance.get('/')
+    return response.data
+}
+export const fetchAirports = async (data) => {
+    const response = await tempAxiosInstance.post('/api/fligts/?type=get_airport_by_lat_long',data)
     return response.data
 }
--- client/src/services/config.js
+++ client/src/services/config.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // const baseURL = import.meta.env.VITE_SERVER_URL;
 const baseURL = "http://127.0.0.1:8000";
+const tempBaseURL = "https://100085.pythonanywhere.com";
 
 if (!baseURL) {
     throw new Error('VITE_SERVER_URL is not defined in the environment variables.');
@@ -10,7 +11,11 @@ if (!baseURL) {
 const servicesAxiosInstance = axios.create({
     baseURL: baseURL
 });
+const tempAxiosInstance = axios.create({
+    baseURL: tempBaseURL
+});
 
 export {
-    servicesAxiosInstance
+    servicesAxiosInstance,
+    tempAxiosInstance
 };
