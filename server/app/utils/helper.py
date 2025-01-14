7ef1ea9353d1f796f8b7a488adbada9635440b75
Manish <mdashsharma95@gmail.com>
2025-01-15 03:08:32 +0530

    feat: Sign up , sign in , airport

---
+++ server/app/utils/helper.py
@@ -0,0 +1,284 @@
+import requests
+import jwt
+import os
+from datetime import datetime, timedelta
+from typing import Dict, Optional
+from functools import wraps
+from django.http import JsonResponse
+import requests
+import time
+import qrcode
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from itertools import chain
+import json
+from functools import partial
+from datetime import datetime, timedelta
+from config.config import *
+
+def dowell_login(workspace_name, user_id, password):
+    url = 'https://100093.pythonanywhere.com/api/portfoliologin'
+    
+    payload = {
+        'portfolio': user_id,
+        'password': password,
+        'workspace_name': workspace_name,
+        "username": "true",
+    }
+
+    try:
+        response = requests.post(url, json=payload)
+        response_data = response.json()
+
+        return {
+            "success": True,
+            "message": "Authentication result",
+            "response": response_data
+        }
+
+    except requests.exceptions.RequestException as req_err:
+        return {
+            "success": False,
+            "message": f"Request failed: {req_err}"
+        }
+
+def get_portfolio_details(workspace_name, portfolio_id):
+    url = 'https://100093.pythonanywhere.com/api/portfoliodetails'
+    payload = {
+        'workspace_name': workspace_name,
+        'portfolio_id': portfolio_id
+    }
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status() 
+        return {
+            "success": True,
+            "message": "Portfolio details retrieved successfully",
+            "response": response.json()["response"]
+        }
+    except requests.exceptions.HTTPError as http_err:
+        return {
+            "success": False,
+            "message": f"Server responded with status code {response.status_code}: {http_err}"
+        }
+    except requests.exceptions.RequestException as req_err:
+        return {
+            "success": False,
+            "message": f"Request failed: {req_err}"
+        }
+    except ValueError as json_err:
+        return {
+            "success": False,
+            "message": f"Error parsing JSON response: {json_err}"
+        }
+    
+def generate_qr_code(url, portfolio_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(url)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill='black', back_color='white').convert('RGB')
+    
+    draw = ImageDraw.Draw(img)
+
+    try:
+        font = ImageFont.truetype('/usr/share/fonts/ttf/dejavu/DejaVuSans-Bold.ttf', 24)
+    except IOError:
+        font = ImageFont.load_default()
+
+    def draw_bottom_centered_text(draw, text, font, img, additional_offset=10):
+        bbox = draw.textbbox((0, 0), text, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_height = bbox[3] - bbox[1]
+        
+        width, height = img.size
+        
+        x = (width - text_width) / 2
+        y = height - text_height - 20 + additional_offset  
+        
+        draw.text((x, y), text, font=font, fill='black')
+
+    draw_bottom_centered_text(draw, portfolio_name, font, img)
+
+    return img
+
+
+def upload_qr_code_image(img, file_name):
+    url = 'https://dowellfileuploader.uxlivinglab.online/uploadfiles/upload-qrcode-to-drive/'
+    with BytesIO() as buffer:
+        img.save(buffer, format='PNG')
+        buffer.seek(0)
+        files = {
+            'file': (file_name, buffer, 'image/png')
+        }
+        try:
+            response = requests.post(url, files=files)
+            response.raise_for_status() 
+            file_url = response.json().get('file_url')
+            return file_url
+        except requests.exceptions.HTTPError as http_err:
+            print(f'Server responded with non-success status: {http_err.response.status_code}')
+        except requests.exceptions.RequestException as req_err:
+            print(f'Error making request: {req_err}')
+        except Exception as err:
+            print(f'Unexpected error: {err}')
+        return None
+
+
+def generate_url(workspace_id, user_id):
+    url = f"{config['FRONTEND_URL']}/dowellflighttracker/?workspace_id={workspace_id}&user_id={user_id}"
+    return url
+
+def generate_file_name(prefix='qrcode', extension='png'):
+    timestamp = int(time.time() * 1000)
+    filename = f'{prefix}_{timestamp}.{extension}'
+    return filename
+
+def get_fligts_data(mode,app_id, app_key, use_headers=False):
+    url = f'https://api.flightstats.com/flex/{mode}/rest/v1/json/active'
+    headers = {}
+    params = {}
+
+    if use_headers:
+        headers = {
+            'appId': app_id,
+            'appKey': app_key,
+            'Content-Type': 'application/json'
+        }
+    else:
+        params = {
+            'appId': app_id,
+            'appKey': app_key
+        }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            return {
+                "success": True,
+                "message": "All airports retrieved successfully",
+                "response": response.json()
+            }
+        else:
+            return {
+                "success": False,
+                "message": f"Request failed with status code: {response.status_code}"
+            }
+    except requests.RequestException as e:
+        return {
+            "success": False,
+            "message":f"Request Exception: {e}"
+        }
+
+
+def get_airports_data_by_lat_long(latitude, longitude,radiusMiles, app_id, app_key, use_headers=False):
+    url = f'https://api.flightstats.com/flex/airports/rest/v1/json/withinRadius/{longitude}/{latitude}/{radiusMiles}'
+
+    params = {
+        'appId': app_id,
+        'appKey': app_key
+    }
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return {
+                "success": True,
+                "message": "All airports retrieved based on lat and long successfully",
+                "response": response.json()
+            }
+        else:
+            return {
+                "success": False,
+                "message": f"Request failed with status code: {response.status_code}"
+            }
+    except requests.RequestException as e:
+        return {
+            "success": False,
+            "message":f"Request Exception: {e}"
+        }
+
+def get_flights_arrival_departure_by_airport(airport_code,type, year,month,day,hourOfDay,maxFlights, app_id, app_key, use_headers=False):
+    url = f"https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/{airport_code}/{type}/{year}/{month}/{day}/{hourOfDay}/?utc=false&numHours=1&maxFlights={maxFlights}"
+
+    print(url)
+
+    params = {
+        'appId': app_id,
+        'appKey': app_key
+    }
+    try:
+        response = requests.get(url, params=params)
+        
+        if response.status_code == 200:
+            return {
+                "success": True,
+                "message": "Data Recived successfully",
+                "response": response.json()
+            }
+        else:
+            return {
+                "success": False,
+                "message": f"Request failed with status code: {response.status_code}"
+            }
+    except requests.RequestException as e:
+        return {
+            "success": False,
+            "message":f"Request Exception: {e}"
+        }
+
+class JWTUtils:
+    def __init__(self):
+        self.secret_key = config['JWT_SECRET_KEY']
+        self.algorithm = config['JWT_ALGORITHM']
+        self.expiry_delta = config['JWT_EXPIRATION_DELTA']
+        self.refresh_expiry_delta = config['JWT_REFRESH_EXPIRATION_DELTA']
+
+    def generate_jwt_tokens(self, data: Dict[str, any]) -> Dict[str, str]:
+        
+        access_payload = {
+            '_id': data["_id"],
+            "workspace_name":data["workspace_name"],
+            'user_id': data["user_id"],
+            'email': data["email"],
+            'workspace_id': data["workspace_id"],
+            'workspace_owner_name': data["workspace_owner_name"],
+            'customer_id': data["customer_id"],
+            'is_active': data["is_active"],
+            'exp': datetime.utcnow() + self.refresh_expiry_delta
+        }
+        access_token = jwt.encode(access_payload, self.secret_key, algorithm=self.algorithm)
+
+        refresh_payload = {
+            '_id': data["_id"],
+            "workspace_name":data["workspace_name"],
+            'user_id': data["user_id"],
+            'email': data["email"],
+            'workspace_id': data["workspace_id"],
+            'workspace_owner_name': data["workspace_owner_name"],
+            'customer_id': data["customer_id"],
+            'is_active': data["is_active"],
+            'exp': datetime.utcnow() + self.refresh_expiry_delta
+        }
+        refresh_token = jwt.encode(refresh_payload, self.secret_key, algorithm=self.algorithm)
+
+        return {
+            'access_token': access_token,
+            'refresh_token': refresh_token
+        }
+
+    def decode_jwt_token(self, token: str) -> Optional[Dict[str, any]]:
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
+            return payload
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+        
+
--- server/app/views.py
+++ server/app/views.py
@@ -6,7 +6,12 @@ import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime, timezone
+from config.config import *
+from .serializers import *
+from .utils.helper import *
+from .services.datacube import *
 
+jwt_utils = JWTUtils()
 @method_decorator(csrf_exempt, name='dispatch')
 class healthCheck(APIView):
     def get(self, request):
@@ -19,3 +24,320 @@ class healthCheck(APIView):
             "server_time": now,
             "message":"API server is UP"
         }, status=status.HTTP_200_OK)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserManagement(APIView):
+    def post(self, request):
+        type = request.GET.get('type')
+        if type == 'signin':
+            return self.signin(request)
+        elif type == 'signup':
+            return self.signup(request)
+        else:
+            return self.handle_error(request)
+        
+    def handle_error(self, request): 
+        return Response({
+            "success": False,
+            "message": "Invalid request type"
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    def signin(self, request):
+        workspace_name = request.data.get("workspace_name")
+        user_id = request.data.get("user_id")
+        password = request.data.get("password")
+
+        serializer = UserAuthSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Posting wrong data to API",
+                "errors": serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        client_admin_login_response = dowell_login(workspace_name, user_id, password)
+
+        if not client_admin_login_response.get("success") or client_admin_login_response.get("response") == 0:
+            return Response({
+                "success": False,
+                "message": client_admin_login_response.get("message", "Authentication failed")
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        
+        data = client_admin_login_response.get("response", {})
+        database = f"{data['userinfo']['owner_id']}_dowell_flight_tracker"
+        user_collection = f"{data['userinfo']['owner_id']}_users"
+
+        existing_user_response = json.loads(datacube_data_retrieval(
+            api_key,
+            database,
+            user_collection,
+            {
+                "workspace_name": workspace_name,
+                "user_id": user_id,
+            },
+            0, 0, False))
+        
+        existing_user = existing_user_response.get('data', [])
+
+        if not existing_user:
+            url = generate_url(data["userinfo"]["owner_id"], user_id)
+            qrcode_image = generate_qr_code(url=url, portfolio_name=user_id)
+
+            file_name = generate_file_name(prefix='qrcode', extension='png')
+
+            qrcode_image_url = None
+
+            try:
+                if config["SAVE_QRCODE"]:
+                    qrcode_image_url = upload_qr_code_image(qrcode_image, file_name)
+                    if not qrcode_image_url:
+                        qrcode_image_url = None
+                else:
+                    save_folder = "qrcodes"
+                    os.makedirs(save_folder, exist_ok=True)
+                    file_path = os.path.join(save_folder, file_name)
+                    qrcode_image.save(file_path)
+                    qrcode_image_url = None
+                
+                if qrcode_image_url and qrcode_image_url.startswith("http://"):
+                    qrcode_image_url = qrcode_image_url.replace("http://", "https://")
+
+            except Exception as err:
+                qrcode_image_url = None
+
+            user_data = {
+                "workspace_name": workspace_name,
+                "user_id": user_id,
+                "email": "",
+                "profile_image": "",
+                "workspace_id": data["userinfo"]["owner_id"],
+                "workspace_owner_name": data["userinfo"]["owner_name"],
+                "customer_id": data["portfolio_info"]["portfolio_name"],
+                "member_type": data["portfolio_info"]["member_type"],
+                "data_type": data["portfolio_info"]["data_type"],
+                "password": password,
+                "latitude": None,
+                "longitude": None,
+                "is_active": True,
+                "is_notification_active": False,
+                "notification_duration": "one_day",
+                "qrcode_image_url": qrcode_image_url,
+                "proximity": 25,
+                "created_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                "records": [{"record": "1", "type": "overall"}]
+            }
+
+            user_data_response = json.loads(datacube_data_insertion(
+                api_key,
+                database,
+                user_collection,
+                user_data
+            ))
+
+            if not user_data_response.get("success"):
+                return Response({
+                    "success": False,
+                    "message": "Failed to create user"
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            data = {
+                "_id": user_data_response["data"]["inserted_id"],
+                "workspace_name": workspace_name,
+                "user_id": user_id,
+                "email": "",
+                "workspace_id": data["userinfo"]["owner_id"],
+                "workspace_owner_name": data["userinfo"]["owner_name"],
+                "customer_id": data["portfolio_info"]["portfolio_name"],
+                "is_active": True,
+            }
+
+            message = "User created successfully"
+        else:
+            existing_user_data = existing_user[0]
+            data = {
+                "_id": existing_user_data["_id"],
+                "workspace_name": existing_user_data["workspace_name"],
+                "user_id": existing_user_data['user_id'],
+                "email": existing_user_data["email"],
+                "workspace_id": existing_user_data["workspace_id"],
+                "workspace_owner_name": existing_user_data["workspace_owner_name"],
+                "customer_id": existing_user_data["customer_id"],
+                "is_active": existing_user_data["is_active"],
+            }
+
+            message = "User authenticated successfully"
+
+        token = jwt_utils.generate_jwt_tokens(data)
+        return Response({
+            "success": True,
+            "message": message,
+            "access_token": token["access_token"],
+            "refresh_token": token["refresh_token"],
+            "response": data
+        })
+    
+    def signup(self, request):
+        user_id = request.data.get("user_id")
+        email = request.data.get("email")
+        latitude = request.data.get("latitude")
+        longitude = request.data.get("longitude")
+        workspace_name= request.data.get("workspace_name")
+
+        serializer = UserSignUpSerializer(data={
+            "email": email,
+            "user_id": user_id,
+            "latitude": latitude,
+            "longitude": longitude,
+            "workspace_name": workspace_name
+        })
+
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Posting wrong data to API",
+                "errors": serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        response = get_portfolio_details(workspace_name, user_id)
+
+        if not response["success"]:
+            return Response({
+                "success": False,
+                "message": response["message"]
+            }, status=status.HTTP_404_NOT_FOUND)
+        
+
+        user_data_response = json.loads(datacube_data_retrieval(
+            api_key, 
+            f"{response['response'][0]['userinfo']['owner_id']}_dowell_flight_tracker", 
+            f"{response['response'][0]['userinfo']['owner_id']}_users", 
+            {
+                "user_id": user_id
+            },
+            1,
+            0,
+            False
+        ))
+
+        data = user_data_response.get("data", [])
+
+        return Response({
+            "success": True,
+            "message": "Email sent successfully",
+            "response": data
+        })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class flight_data(APIView):
+
+    def post(self, request):
+        type_request = request.GET.get('type')
+
+        if type_request == "get_airport_by_lat_long":
+            return self.get_airport_by_lat_long(request)
+        elif type_request == "get_flights_arrival_departure":
+            return self.get_flights_arrival_departure(request)
+        else:
+            return self.handle_error(request)
+    def get(self, request):
+        type_request = request.GET.get('type')
+
+        if type_request == "get_airports":
+            return self.get_airports(request)
+        elif type_request == "get_airlines":
+            return self.get_airlines(request)
+        else:
+            return self.handle_error(request)
+        
+    def get_airports(self, request):
+        res = get_fligts_data('airports',config["FLIGHT_SERVICE_APP_ID"],config["FLIGHT_SERVICE_APP_KEY"])
+
+        if not res['success']:
+            return Response({
+                "success": False,
+                "message": res['message']
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({
+            "success": True,
+            "message": "All Airports list retrieved successfully",
+            "response": res['response']['airports']  
+        })
+    
+    def get_airlines(self, request):
+        res = get_fligts_data('airlines',config["FLIGHT_SERVICE_APP_ID"],config["FLIGHT_SERVICE_APP_KEY"])
+
+        if not res['success']:
+            return Response({
+                "success": False,
+                "message": res['message']
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        print(len(res['response']['airlines']))
+        return Response({
+            "success": True,
+            "message": "All Airports list retrieved successfully",
+            "response": res['response']['airlines']  
+        })
+
+    def get_airport_by_lat_long(self, request):
+        latitude = request.data.get('latitude')
+        longitude = request.data.get('longitude')
+        radiusMiles = request.data.get('radiusMiles')
+
+        if not all([latitude, longitude,radiusMiles]):
+            return Response({
+                "success": False,
+                "message": "Please provide both latitude and longitude"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        res = get_airports_data_by_lat_long(latitude, longitude, radiusMiles, config["FLIGHT_SERVICE_APP_ID"],config["FLIGHT_SERVICE_APP_KEY"])
+
+        if not res['success']:
+            return Response({
+                "success": False,
+                "message": res['message']
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({
+            "success": True,
+            "message": "All Airports list retrieved successfully",
+            "response": res['response']['airports']
+        })
+    
+    def get_flights_arrival_departure(self, request):
+        airport_code = request.data.get('airport_code')
+        year = request.data.get('year')
+        month = request.data.get('month')
+        day = request.data.get('day')
+        hourOfDay = request.data.get('hourOfDay')
+        maxFlights = request.data.get('maxFlights')
+        typeOfStatus = request.data.get('typeOfStatus')
+
+        if not all([airport_code, year, month, day, hourOfDay, maxFlights,typeOfStatus]):
+            return Response({
+                "success": False,
+                "message": "Please provide all required parameters"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        res = get_flights_arrival_departure_by_airport(airport_code,typeOfStatus, year, month, day, hourOfDay, maxFlights, config["FLIGHT_SERVICE_APP_ID"],config["FLIGHT_SERVICE_APP_KEY"])
+
+        if not res['success']:
+            return Response({
+                "success": False,
+                "message": res['message']
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({
+            "success": True,
+            "message": "All Airports list retrieved successfully",
+            "response": res['response']['flightStatuses']
+        })
+
+    
+    def handle_error(self, request): 
+        return Response({
+            "success": False,
+            "message": "Invalid request type"
+        }, status=status.HTTP_400_BAD_REQUEST)
